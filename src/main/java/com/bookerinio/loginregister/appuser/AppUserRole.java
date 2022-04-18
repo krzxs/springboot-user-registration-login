f752728ba3c9798a77c3f7a02c4cd2fbd29ec05e
krzxs <103908619+krzxs@users.noreply.github.com>
2022-04-18 02:44:17 +0200

    Init commit

---
+++ src/main/java/com/bookerinio/loginregister/appuser/AppUserRole.java
@@ -0,0 +1,5 @@
+package com.bookerinio.loginregister.appuser;
+
+public enum AppUserRole {
+    USER, ADMIN;
+}
---
+++ src/main/java/com/bookerinio/loginregister/email/EmailSender.java
@@ -0,0 +1,7 @@
+package com.bookerinio.loginregister.email;
+
+public interface EmailSender {
+
+    void send(String to, String email);
+
+}
